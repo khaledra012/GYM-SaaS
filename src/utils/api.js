bba763e23efac8c1ff792fea1a9f2c2cbabeb83f
unknown <khaledrandan734@gmail.com>
2026-03-10 13:17:57 +0200

    update

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:3000/api/v1';
+const BASE_URL = 'https://gym-saas-backend.railway.internal/api/v1';
 
 const getAuthHeader = () => {
     const token = localStorage.getItem('token');
